71f26f7816f5f9117651a35f3b4cc95689c31e8c
ThullioF <thullioferreira@hotmail.com>
2024-02-26 17:53:36 -0300

    First

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Aluno obj1 = new Aluno(15,17,"João",6,5);
-
-        obj1.notaFinal();
-        obj1.dadosAlunos();
-        obj1.passou();
-    }
-}
