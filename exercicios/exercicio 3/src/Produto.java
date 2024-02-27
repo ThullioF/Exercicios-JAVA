92a8fc305889a547651a7f39f523cfb5b95fb5ed
ThullioF <thullioferreira@hotmail.com>
2024-02-27 10:16:14 -0300

    First

---
+++ exercicios/exercicio 3/src/Produto.java
@@ -0,0 +1,44 @@
+public class Produto {
+    public int id, qtde;
+    public String descricao;
+    public float preco;
+    public Produto(int id, int qtde, String descricao, float preco) {
+        this.id = id;
+        this.qtde = qtde;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+    public Produto() {
+    }
+    public void comprar(int x){
+        this.qtde += x;
+    }
+    public void vender(int x){
+        if (this.qtde - x >= 0){
+            this.qtde -= x;
+        }
+        else {
+            System.out.println("Produto em falta no estoque");
+        }
+    }
+    public void subir(float x){
+        this.preco += x;
+    }
+    public void descer(float x){
+        if (this.preco - x >= 0){
+            this.preco -= x;
+        }
+        else {
+            System.out.println("");
+        }
+    }
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", qtde=" + qtde +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
