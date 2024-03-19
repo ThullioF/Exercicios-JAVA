a263b146b612f4482a5d756b28645ea5d84a0fba
ThullioF <thullioferreira@hotmail.com>
2024-03-18 22:10:51 -0300

    exemplo

--- Heranca/src/Main.java
+++ Heranca/src/Main.java
@@ -1,5 +1,19 @@
+import rh.*;
 public class Main {
-    public static void main(String[] args) {
 
+    public static void exibeDados(Funcionario camaelao){
+        System.out.println(camaelao.toString());
+        System.out.println("Sálario final " + camaelao.calculaSalario());
+    }
+
+    public static void main(String[] args) {
+        Assistente as1 = new Assistente("Fulano", "Franca","123", 3000,10);
+        exibeDados(as1);
+
+        Gerente ge1 = new Gerente("Ciclano", "Franca", "321", 5000,2000);
+        exibeDados(ge1);
+
+        Diretor di1 = new Diretor("Beltrano","Franca","456",10000,1000);
+        exibeDados(di1);
     }
 }
--- Heranca/src/rh/Assistente.java
+++ Heranca/src/rh/Assistente.java
@@ -7,6 +7,25 @@ public class Assistente extends Funcionario {
     }
     public Assistente(String nome, String endereco, String cpf, float salario, float horaExtra) {
         super(nome, endereco, cpf, salario);//chama o construtor da classe pai
+        this.setHoraExtra(horaExtra);
+    }
+
+    public float getHoraExtra() {
+        return horaExtra;
+    }
+    public void setHoraExtra(float horaExtra) {
         this.horaExtra = horaExtra;
+    }
+
+    @Override
+    public String toString() {
+        return "Assistente{" +
+                super.toString() + "Hora extra= " + horaExtra +
+                '}';
+    }
+
+    @Override
+    public float calculaSalario(){
+        return super.calculaSalario() + this.horaExtra * 50;
     }
 }
